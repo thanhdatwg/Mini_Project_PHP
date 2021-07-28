b0f2d1a8b3d7f35fdf8d68a7950fcdab804de3da
Anh Trung <trung.lha@AnhTrungs-MacBook-Air.local>
2021-07-28 17:54:28 +0700

    refactor code

---
+++ Models/Products.php
@@ -0,0 +1,5 @@
+<?php
+
+class Models_Products extends Apps_Libs_DbConnection {
+    protected $tableName = "products";
+}
--- index.php
+++ index.php
@@ -1,32 +1,59 @@
+<?php
+
+session_start();
+$conn = mysqli_connect('localhost', 'root', 'root' , 'Mini_project_php') or die ('Unable to connect');
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
-        body {
+        body{
             text-align: center;
         }
 
-        .field {
+        .field{
             margin-bottom: 20px;
         }
     </style>
+    
 </head>
-
 <body>
-
+    
     <h2>Please Login</h2>
     <div>
-        <form action="index.php" method="post">
-            <input type="text" class="field" name="Username" placeholder="Username" required=""><br />
-            <input type="password" class="field" name="Pass" placeholder="Password" required=""><br />
-            <input type="submit" class="field" name="login" value="Login">
+        <form action = "index.php" method = "post">
+            <input type = "text" class = "field" name = "Username" placeholder = "Username" required = ""><br/>
+            <input type = "password" class = "field" name = "Pass" placeholder = "Password" required = ""><br/>
+            <input type = "submit" class = "field" name = "login" value = "Login">
         </form>
     </div>
 
+<?php
+    if (isset($_POST['login'])){
+        $Username = $_POST['Username'];
+        $Pass = $_POST['Pass'];
+
+    $select = mysqli_query($conn," SELECT * FROM tb_student WHERE Username = '$Username' AND Pass = '$Pass' ");
+    $row  = mysqli_fetch_array($select);
+
+    if(is_array($row)) {
+        $_SESSION["Username"] = $row['Username'];
+        $_SESSION["Pass"] = $row['Pass'];
+    }   else {
+        echo '<script type = "text/javascript">';
+        echo 'alert("Invalid Username or Password!");';
+        echo 'window.location.href = "index.php" ';
+        echo '</script>';
+    }
+    }
+    if(isset($_SESSION["Username"])){
+        header("Location:Views/home.php");
+    }
+?>
 
 </body>
-
 </html>
