b77685b015df28fadf04711c846c2fb512a59226
thanhdat <thanhdatwg@gmail.com>
2021-08-01 12:44:07 +0700

    fix style project

--- views/home.php
+++ views/home.php
@@ -16,9 +16,7 @@ $allProducts = $modelProducts->buildQueryParams([
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
-        body {
-            text-align: center;
-        }
+
     </style>
     <link rel="stylesheet" href="../CSS/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -31,89 +29,19 @@ $allProducts = $modelProducts->buildQueryParams([
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
-<style>
-
-/* The popup form - hidden by default */
-.form-popup {
-  display: none;
-  position: relative;
-  right: 15px;
-  border: 3px solid #f1f1f1;
-  margin-top: 40px;
-}
-
-/* Add styles to the form container */
-.form-container {
-  position: static;
-  max-width: 700px;
-  padding: 20px;
-  background-color: white;
-  margin-left: 25%;
-}
-
-/* Full-width input fields */
-.form-container input[type=text], .form-container input[type=number] {
-  width: 100%;
-  padding: 15px;
-  margin: 5px 0 22px 0;
-  border: none;
-  background: #f1f1f1;
-}
-
-/* When the inputs get focus, do something */
-.form-container input[type=text]:focus, .form-container input[type=password]:focus {
-  background-color: #ddd;
-  outline: none;
-}
-
-/* Set a style for the submit/login button */
-.form-container .btn {
-  background-color: #04AA6D;
-  color: white;
-  padding: 16px 20px;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-  margin-bottom:10px;
-  opacity: 0.8;
-}
-
-/* Add a red background color to the cancel button */
-.form-container .cancel {
-  background-color: red;
-}
-
-/* Add some hover effects to buttons */
-.form-container .btn:hover, .open-button:hover {
-  opacity: 1;
-}
-</style>
 
 <body>
 
-    <h2>Welcome <?php echo $_SESSION['username']; ?></h2>
-    <div align="center">
-        <?php if (empty($_SESSION["username"])) {
-
-            header('location:index.php')
-
-        ?>
-
-
-
-        <?php } else { ?>
-
-            <div class="member-dashboard">You have Successfully logged in!. <a href="logout.php" style="color: #fff">Logout</a></div>
-
-        <?php } ?>
-
+    <div class="header">
+        <a href="#default" class="logo">Mini Project PHP</a>
+        <div class="header-right">
+            <a class="active" href="logout.php">Logout</a>
+        </div>
     </div>
-    Click here to <a href="logout.php">Logout</a>
-    <br />
-    <p></p>
+    <h2>Welcome <?php echo $_SESSION['username']; ?></h2>
     <div>
-        <input type="search" placeholder="Enter a name to searching"/>
-        <button class="btn" onclick="show_hide()">Add Product</button>
+        <input style="width: 150px;height: 33px;" type="search" placeholder="Enter a name to searching" />
+        <button class="btn btn-primary" onclick="show_hide()">Add Product</button>
         <div class="form-popup" id="add-form">
             <form class="form-container" method="post">
                 <h1>Thêm mới sản phẩm</h1>
@@ -127,19 +55,19 @@ $allProducts = $modelProducts->buildQueryParams([
                 <label for="quantity"><b>Số lượng</b></label>
                 <input type="number" placeholder="Nhập số lượng" name="quantity" id="quantity" required>
 
-                <button type="submit" class="btn" id="add_button">Thêm mới</button>
-                <button type="button" class="btn cancel" onclick="show_hide()" >Đóng</button>
+                <button type="submit" class="btn btn-primary" id="add_button">Submit</button>
+                <button type="button" class="btn cancel" onclick="show_hide()">Cancel</button>
             </form>
         </div>
     </div>
     <table class="customers">
         <thead>
             <tr>
-                <th>Mã sản phẩm</th>
-                <th>Tên sản phẩm</th>
-                <th>Số lượng</th>
-                <th>Giá</th>
-                <th>Hành động</th>
+                <th>Product code</th>
+                <th>Product's name</th>
+                <th>Amount</th>
+                <th>Price</th>
+                <th style="display: flex; justify-content: center">Action</th>
             </tr>
         </thead>
         <tbody>
@@ -148,12 +76,12 @@ $allProducts = $modelProducts->buildQueryParams([
             foreach ($allProducts as $row => $product) {
                 echo '
                     <tr>
-                        <td>'. $product["id"].'</td>
-                        <td>'. $product["name"] .'</td>
-                        <td>'. $product["price"].'</td>
-                        <td>'. $product["quantity"] .'</td>
-                        <td><button class="btn btn-update">Update</button>
-                        <button class="btn btn-delete">Delete</button></td>
+                        <td>' . $product["id"] . '</td>
+                        <td>' . $product["name"] . '</td>
+                        <td>' . $product["price"] . '</td>
+                        <td>' . $product["quantity"] . '</td>
+                        <td style="display: flex;justify-content: space-evenly"><button class="btn btn-primary">Update</button>
+                        <button class="btn btn-danger">Delete</button></td>
                     </tr>
                 ';
             }
@@ -162,19 +90,23 @@ $allProducts = $modelProducts->buildQueryParams([
     </table>
     <script src="../Apps/JS/showHideElement.js"></script>
     <script type="text/javascript">
-        $('#add_button').on('click',function (){
+        $('#add_button').on('click', function() {
             var name = $('#name').val();
             var price = $('#price').val();
             var quantity = $('#quantity').val();
 
-            if (name== '' || price=='' || quantity ==''){
+            if (name == '' || price == '' || quantity == '') {
                 alert('khong duoc bo trong cac tuong');
             } else {
                 $.ajax({
                     url: "../Apps/Libs/ajax.php",
                     method: "POST",
-                    data: {name:name, price:price, quantity:quantity},
-                    success : function (data) {
+                    data: {
+                        name: name,
+                        price: price,
+                        quantity: quantity
+                    },
+                    success: function(data) {
                         alert('truyen thanh cong');
                     },
                 });
